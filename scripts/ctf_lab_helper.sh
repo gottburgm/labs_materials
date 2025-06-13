359befcb93ac487a4af940c1c6a37e6479b5b9ac
gottburgm <mikagott@hotmail.com>
2025-06-13 14:20:07 +0200

    -

--- scripts/ctf_lab_helper.sh
+++ scripts/ctf_lab_helper.sh
@@ -22,9 +22,9 @@ SCRIPT
 
 
      export NMAP_BASE_SCAN=$(cat<<SCRIPT
-\#\!/bin/bash
+#!/bin/bash
 
-sudo nmap -v -sT -sV -O -A --open --reason -Pn -p- -T4 -vvv ${TARGET} -oA ${TARGET}_tcp_scan
+sudo nmap -v -sT -sV -O -A --open --reason -Pn -p- -T4 -vvv ${TARGET} --script "smb2-capabilities.nse,smb2-security-mode.nse,smb2-time.nse,smb2-vuln-uptime.nse,smb-double-pulsar-backdoor.nse,smb-enum-domains.nse,smb-enum-groups.nse,smb-enum-processes.nse,smb-enum-services.nse,smb-enum-sessions.nse,smb-enum-shares.nse,smb-enum-users.nse,smb-ls.nse,smb-mbenum.nse,smb-os-discovery.nse,smb-print-text.nse,smb-protocols.nse,smb-psexec.nse,smb-security-mode.nse,smb-server-stats.nse,smb-system-info.nse,smb-vuln-conficker.nse,smb-vuln-cve2009-3103.nse,smb-vuln-cve-2017-7494.nse,smb-vuln-ms06-025.nse,smb-vuln-ms07-029.nse,smb-vuln-ms08-067.nse,smb-vuln-ms10-054.nse,smb-vuln-ms10-061.nse,smb-vuln-ms17-010.nse,smb-vuln-webexec.nse,smb-webexec-exploit.nse" -oA ${TARGET}_tcp_scan
 SCRIPT
 )
 
